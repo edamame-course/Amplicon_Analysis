81a3ec726a296cd6e9ace973c71a86cec0b3d80f
Jackson Sorensen <jacksonwsorensen@gmail.com>
2016-05-26 16:18:32 -0400

    added cleanup step

--- resources/Merged_Reads_Script.sh
+++ resources/Merged_Reads_Script.sh
@@ -2,4 +2,5 @@ for file in $(<list2.txt)
 do
     join_paired_ends.py -f ${file}F_sub.fastq -r ${file}R_sub.fastq -o ${file}/
     mv ${file}/fastqjoin.join.fastq Merged_Reads/${file}_merged.fastq
+    rm -r ${file}/
 done
